9d29b51976cb413c857b5e5cacd55f97e438f910
cammori <cammori@gmail.com>
2015-03-31 11:50:18 -0400

    Updates

--- src/com/gc/TokenizeSentences.java
+++ src/com/gc/TokenizeSentences.java
@@ -1,66 +1,66 @@
 package com.gc;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
-import java.util.Properties;
 
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.process.PTBTokenizer;
 
 public class TokenizeSentences {
-//
-public static void main(String[] args) {
-//		String paragraph = "My 1st sentence. “Does it work for questions?” My third sentence.";
-//		Reader reader = new StringReader(paragraph);
-//		DocumentPreprocessor dp = new DocumentPreprocessor(reader);
-//		List<String> sentenceList = new ArrayList<String>();
-//		for (List<HasWord> sentence : dp) {
-//			String sentenceString = Sentence.listToString(sentence);
-//			sentenceList.add(sentenceString.toString());
-//		}
-//
-//		for (String sentence : sentenceList) {
-//			System.out.println(sentence);
-//		}
-//	}
 
-//creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution 
-Properties props = new Properties();
-props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
-StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-
-//read some text in the text variable
-String text = "";
-
-//create an empty Annotation just with the given text
-Annotation document = new Annotation(text);
-
-//run all Annotators on this text
-pipeline.annotate(document);
-
-//these are all the sentences in this document
-//a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-
-for(CoreMap sentence: sentences) {
-// traversing the words in the current sentence
-// a CoreLabel is a CoreMap with additional token-specific methods
-for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
- // this is the text of the token
- String word = token.get(TextAnnotation.class);
- // this is the POS tag of the token
- String pos = token.get(PartOfSpeechAnnotation.class);
- // this is the NER label of the token
- String ne = token.get(NamedEntityTagAnnotation.class);  
- System.out.println(word);
- System.out.println(pos);
- System.out.println(ne);
+  public static void main(String[] args) throws IOException {
+    for (String arg : args) {
+      // option #1: By sentence.
+//      DocumentPreprocessor dp = new DocumentPreprocessor(arg);
+//      for (List sentence : dp) {
+//        System.out.println(sentence);
+//      }
+      // option #2: By token
+      PTBTokenizer ptbt = new PTBTokenizer(new FileReader(arg),
+              new CoreLabelTokenFactory(), "");
+      for (CoreLabel label; ptbt.hasNext(); ) {
+        label = ptbt.next();
+        System.out.println(label);
+      }
+    }
+  }
 }
-}
-}}
+// //creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER,
+// parsing, and coreference resolution
+// Properties props = new Properties();
+// props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+// StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+//
+// //read some text in the text variable
+// String text = "";
+//
+// //create an empty Annotation just with the given text
+// Annotation document = new Annotation(text);
+//
+// //run all Annotators on this text
+// pipeline.annotate(document);
+//
+// //these are all the sentences in this document
+// //a CoreMap is essentially a Map that uses class objects as keys and has
+// values with custom types
+// List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+//
+// for(CoreMap sentence: sentences) {
+// // traversing the words in the current sentence
+// // a CoreLabel is a CoreMap with additional token-specific methods
+// for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+// // this is the text of the token
+// String word = token.get(TextAnnotation.class);
+// // this is the POS tag of the token
+// String pos = token.get(PartOfSpeechAnnotation.class);
+// // this is the NER label of the token
+// String ne = token.get(NamedEntityTagAnnotation.class);
+// System.out.println(word);
+// System.out.println(pos);
+// System.out.println(ne);
+// }
+
