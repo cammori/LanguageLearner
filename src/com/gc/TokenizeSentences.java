a93b8708077b3675ce7ec249c68e3e91b82f3827
cammori <cammori@gmail.com>
2015-03-31 11:04:07 -0400

    Added main class

--- src/com/gc/TokenizeSentences.java
+++ src/com/gc/TokenizeSentences.java
@@ -3,7 +3,6 @@ package com.gc;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import edu.stanford.nlp.ling.HasWord;
@@ -11,24 +10,27 @@ import edu.stanford.nlp.ling.Sentence;
 import edu.stanford.nlp.process.DocumentPreprocessor;
 
 public class TokenizeSentences {
-	String paragraph = "My 1st sentence. “Does it work for questions?” My third sentence.";
-	Reader reader = new StringReader(paragraph);
-	DocumentPreprocessor dp = new DocumentPreprocessor(reader);
-	List <String> sentenceList = new ArrayList<String>();
 
-	for (List<HasWord> sentence1 : dp) {
-	   String sentenceString = Sentence.listToString(sentence1);
-	   sentenceList.add(sentenceString.toString());
-	}
+	public static void main(String[] args) {
+		String paragraph = "My 1st sentence. “Does it work for questions?” My third sentence.";
+		Reader reader = new StringReader(paragraph);
+		DocumentPreprocessor dp = new DocumentPreprocessor(reader);
+		List<String> sentenceList = new ArrayList<String>();
+		for (List<HasWord> sentence : dp) {
+			String sentenceString = Sentence.listToString(sentence);
+			sentenceList.add(sentenceString.toString());
+		}
 
-	for (String sentence : sentenceList) {
-	   System.out.println(sentence1);
+		for (String sentence : sentenceList) {
+			System.out.println(sentence);
+		}
+
+		// HashMap<Integer, String> tokenizedSentences = new HashMap<Integer,
+		// String>();
+		// public String[] sentenceTokenizer(String HTMLParagraphs) {
+		// //Take HTMLParagraphs to
+		// return null;
+		//
+		// }
 	}
-	
-	// 	HashMap<Integer, String> tokenizedSentences = new HashMap<Integer, String>();
-	// public String[] sentenceTokenizer(String HTMLParagraphs) {
-//		//Take HTMLParagraphs to 
-//		return null;
-//
-//	}
-}}
+}
