86e2c1815407929721d9af6f2f6255ed2b5133a3
cammori <cammori@gmail.com>
2015-03-31 13:23:19 -0400

    Same Update

--- src/com/gc/TokenizeSentences.java
+++ src/com/gc/TokenizeSentences.java
@@ -1,6 +1,6 @@
 package com.gc;
 
-public class TokenizeSentences{
+public class TokenizeSentences {
 	public static void main(String[] args) {
 		String str = "This is how I tried to split a paragraph into a sentence. But, there is a problem. My paragraph includes dates like Jan. 13, 2014, words like U.S. and numbers like 2.2 at the end of the sentence. They all got splitted by the above code.";
 		String[] sentences = str.split("(?<=[.?!])\\s+(?=[a-zA-Z])");
