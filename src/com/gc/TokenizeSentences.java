c2088761b58586542297ffc7d0309c0cb4eac34a
cammori <cammori@gmail.com>
2015-03-31 13:44:52 -0400

    Double checking it works

--- src/com/gc/TokenizeSentences.java
+++ src/com/gc/TokenizeSentences.java
@@ -2,10 +2,10 @@ package com.gc;
 
 public class TokenizeSentences {
 	public static void main(String[] args) {
-		String str = "This is how I tried to split a paragraph into a sentence. But, there is a problem. My paragraph includes dates like Jan. 13, 2014, words like U.S. and numbers like 2.2 at the end of the sentence. They all got splitted by the above code.";
-		String[] sentences = str.split("(?<=[.?!])\\s+(?=[a-zA-Z])");
-		for (int i = 0; i < sentences.length; i++) {
-			System.out.println(sentences[i]);
+		String htmlInput = "This is how I tried to split a paragraph into a sentence. But, there is a problem. My paragraph includes dates like Jan. 13, 2014, words like U.S. and numbers like 2.2 at the end of the sentence. What if I have \"says this person\" within? They all got splitted by the above code.";
+		String[] tokenizedSentences = htmlInput.split("(?<=[.?!])\\s+(?=[a-zA-Z])");
+		for (int i = 0; i < tokenizedSentences.length; i++) {
+			System.out.println(tokenizedSentences[i]);
 		}
 	}
 }
